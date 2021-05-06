f230f8b69a8ae7adb4f214705a5a33b5ecd190c9
MingzheLiu1998 <lmz199824@gmail.com>
2021-05-06 17:33:33 +0800

    2021_05_06_update

---
+++ 双指针/快排模板.py
@@ -0,0 +1,32 @@
+#快排先整体有序，再局部有序。 
+#先让[start, right] & [left, end]有序
+#(左边>= pivot，右边 < pivot), 之后再让两个区间局部有序
+
+def quickSort(self, nums):
+	if not nums:
+		return nums 
+	self.quickSortHelper(nums, 0, len(nums) - 1)
+
+
+def quickSortHelper(self, nums, start, end):
+	if start >= end:
+		return 
+	pivot = nums[start + (end - start) // 2]
+	left, right = start, end 
+
+	while left <= right:
+		while left <= right and nums[left] < pivot:
+			left += 1
+		while left <= right and nums[right] > pivot: #注意nums[right] == pivot时候，R指针停止，等待交换
+			right -= 1
+		if left <= right: #互换
+			nums[left], nums[right] == nums[right], nums[left]
+			left += 1
+			right -= 1
+
+	self.quickSortHelper(nums, start, right)
+	self.quickSortHelper(nums, left, end)
+
+
+
+
